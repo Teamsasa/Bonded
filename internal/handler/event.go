8080147283b30098e36305658d46d4f409f52aef
Yutosaki <sasayu8190@gmail.com>
2024-12-02 19:35:52 +0900

    delete: 使用していない関数

--- internal/handler/event.go
+++ internal/handler/event.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 
 	"bonded/internal/models"
@@ -52,45 +53,5 @@ func (h *Handler) HandleCreateEvent(ctx context.Context, request events.APIGatew
 	return events.APIGatewayProxyResponse{
 		StatusCode: 201,
 		Body:       `{"message":"Event created successfully.", "eventId":"` + event.EventID + `"}`,
-	}, nil
-}
-
-func (h *Handler) HandleGetEvents(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	calendarID := request.PathParameters["calendarId"]
-
-	// カレンダーの存在確認
-	calendar, err := h.CalendarUsecase.FindCalendar(ctx, calendarID)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Error finding calendar: " + err.Error(),
-		}, nil
-	}
-	if calendar == nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 404,
-			Body:       "Calendar not found",
-		}, nil
-	}
-
-	// イベントを取得
-	eventList, err := h.EventUsecase.FindEvents(ctx, calendarID) // 変数名を変更
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Error finding events: " + err.Error(),
-		}, nil
-	}
-
-	body, err := json.Marshal(eventList)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Error marshalling response: " + err.Error(),
-		}, nil
-	}
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(body),
 	}, nil
 }

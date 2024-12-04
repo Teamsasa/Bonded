ad5237fe8fe638a783dadffbc51bbb89b7e7f9ee
Yutosaki <sasayu8190@gmail.com>
2024-12-04 14:38:48 +0900

    feat: modelを訂正

--- internal/models/event.go
+++ internal/models/event.go
@@ -1,13 +1,11 @@
 package models
 
-import "time"
-
 type Event struct {
-    EventID     string    `json:"eventId" dynamodbav:"EventID"`       // イベントID
-    Title       string    `json:"title" dynamodbav:"Title"`           // イベント名
-    Description string    `json:"description" dynamodbav:"Description"` // 詳細
-    StartTime   time.Time `json:"startTime" dynamodbav:"StartTime"`   // 開始時間
-    EndTime     time.Time `json:"endTime" dynamodbav:"EndTime"`       // 終了時間
-    Location    string    `json:"location" dynamodbav:"Location"`     // 場所
-    AllDay      bool      `json:"allDay" dynamodbav:"AllDay"`         // 終日フラグ
+	EventID     string `json:"eventId" dynamodbav:"EventID"`         // イベントID
+	Title       string `json:"title" dynamodbav:"Title"`             // イベント名
+	Description string `json:"description" dynamodbav:"Description"` // 詳細
+	StartTime   string `json:"startTime" dynamodbav:"StartTime"`     // 開始時間
+	EndTime     string `json:"endTime" dynamodbav:"EndTime"`         // 終了時間
+	Location    string `json:"location" dynamodbav:"Location"`       // 場所
+	AllDay      bool   `json:"allDay" dynamodbav:"AllDay"`           // 終日フラグ
 }

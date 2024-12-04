65c2ce131ac9a1b32f03d0c70a052eed6693960d
Yutosaki <sasayu8190@gmail.com>
2024-12-04 19:58:31 +0900

    fix: formatter

--- internal/models/calendar.go
+++ internal/models/calendar.go
@@ -4,7 +4,7 @@ type Calendar struct {
 	CalendarID  string  `json:"calendarId,omitempty" dynamodbav:"CalendarID"`   // カレンダーのID
 	SortKey     string  `json:"sortKey,omitempty" dynamodbav:"SortKey"`         // ソートキー
 	Name        string  `json:"name" dynamodbav:"Name"`                         // カレンダー名
-	IsPublic    *bool    `json:"isPublic" dynamodbav:"IsPublic"`                 // 公開フラグ
+	IsPublic    *bool   `json:"isPublic" dynamodbav:"IsPublic"`                 // 公開フラグ
 	OwnerUserID string  `json:"ownerUserId,omitempty" dynamodbav:"OwnerUserID"` // オーナーのユーザーID
 	Users       []User  `json:"users,omitempty" dynamodbav:"Users"`             // 共有ユーザーのIDリスト
 	Events      []Event `json:"events,omitempty"`                               // カレンダー内のイベント
